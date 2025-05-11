8d060ead28ca6f19475c18d118772c9a4c23fd6c
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-11 18:47:12 +0100

    dataabases

---
+++ database/__init__.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from constants import SQLALCHEMY_DATABASE_URI
+
+# the below is the connection to the database, the connect_args is used to avoid error
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+Db_Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+
+# The function below is used to get a database session
+def get_db():
+    db = Db_Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# to upgrade the database
+# alembic upgrade head
