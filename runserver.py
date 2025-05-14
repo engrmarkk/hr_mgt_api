757c8a80e1b6b3a7a2dd11388d1bbb2d8ddd82a5
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-14 15:01:32 +0100

    Create runserver.py

---
+++ runserver.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import os
+
+    os.system(
+        "uvicorn settings:create_app --host 0.0.0.0 --port 8000 --reload --factory"
+        # "uvicorn app_config:create_app --host 0.0.0.0 --port 8000 --workers 4 --factory"
+    )
