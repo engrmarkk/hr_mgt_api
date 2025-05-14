5ca45398b48370deb15a9e77ece34248505f7de5
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-14 15:01:51 +0100

    Update rate_limit.py

--- utils/rate_limit.py
+++ utils/rate_limit.py
@@ -1,8 +1,9 @@
-from func import get_user_id_from_request
+from cruds import get_user_id_from_request
 from slowapi import Limiter
-from constants import REDIS_HOST, REDIS_PORT
+from constants import REDIS_HOST, REDIS_PORT, REDIS_URL
 
 limiter = Limiter(
     key_func=get_user_id_from_request,
-    storage_uri=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    # storage_uri=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    storage_uri=REDIS_URL,
 )
