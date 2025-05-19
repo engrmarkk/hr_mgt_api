d0a19a122e5968173faa7c9dee7009ead9f3468d
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-19 16:50:00 +0100

    Update __init__.py

--- apis/misc/__init__.py
+++ apis/misc/__init__.py
@@ -1,33 +1,33 @@
-from fastapi import APIRouter, status, Depends, HTTPException
-from database import get_db
-from sqlalchemy.orm import Session
-from logger import logger
-from schemas import MiscMenuSchema
-from models import SideMenu
-from constants import EXCEPTION_MESSAGE
-
-misc_router = APIRouter(prefix="/misc", tags=["Miscellaneous"])
-
-
-@misc_router.post("/side_menu", status_code=status.HTTP_200_OK)
-async def create_side_menu(menu_data: MiscMenuSchema, db: Session = Depends(get_db)):
-    try:
-        name = menu_data.name
-        description = menu_data.description
-        icon_url = menu_data.icon_url
-        tag = menu_data.tag
-
-        side_menu = SideMenu(name=name, description=description, icon=icon_url, tag=tag)
-        db.add(side_menu)
-        db.commit()
-        db.refresh(side_menu)
-        return {"detail": "Side menu created successfully", "id": side_menu.id}
-    except HTTPException as http_exc:
-        # Log the HTTPException if needed
-        logger.exception("traceback from login")
-        raise http_exc
-    except Exception as e:
-        logger.exception("traceback from login")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=EXCEPTION_MESSAGE
-        )
+# from fastapi import APIRouter, status, Depends, HTTPException
+# from database import get_db
+# from sqlalchemy.orm import Session
+# from logger import logger
+# from schemas import MiscMenuSchema
+# # from models import SideMenu
+# from constants import EXCEPTION_MESSAGE
+#
+# misc_router = APIRouter(prefix="/misc", tags=["Miscellaneous"])
+#
+#
+# # @misc_router.post("/side_menu", status_code=status.HTTP_200_OK)
+# # async def create_side_menu(menu_data: MiscMenuSchema, db: Session = Depends(get_db)):
+# #     try:
+# #         name = menu_data.name
+# #         description = menu_data.description
+# #         icon_url = menu_data.icon_url
+# #         tag = menu_data.tag
+# #
+# #         side_menu = SideMenu(name=name, description=description, icon=icon_url, tag=tag)
+# #         db.add(side_menu)
+# #         db.commit()
+# #         db.refresh(side_menu)
+# #         return {"detail": "Side menu created successfully", "id": side_menu.id}
+# #     except HTTPException as http_exc:
+# #         # Log the HTTPException if needed
+# #         logger.exception("traceback from login")
+# #         raise http_exc
+# #     except Exception as e:
+# #         logger.exception("traceback from login")
+# #         raise HTTPException(
+# #             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=EXCEPTION_MESSAGE
+# #         )
