7b1ab1fc40ef97863d2e889aeec032a75418ce34
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-26 09:41:57 +0100

    Update __init__.py

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -4,6 +4,9 @@ from database import get_db
 from sqlalchemy.orm import Session
 from security import get_current_user
 from models import Users
+from logger import logger
+from connections import redis_conn
+import json
 
 
 def email_verified():
@@ -25,4 +28,26 @@ def email_verified():
 
         return wrapper
 
+    return decorator
+
+
+# get the result from the  decorator
+def cache_it(red_key: str, org: bool = False, user: bool = False):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(
+            *args,
+            **kwargs
+        ):
+            current_user = kwargs.get('current_user')
+            redkey = f"{red_key}:{current_user.id}" if user else (
+                f"{red_key}:{current_user.organization_id}" if org else red_key)
+            result = redis_conn.get(redkey)
+            if result:
+                logger.info("get result from decorator redis")
+                return json.loads(result)
+            result = await func(*args, **kwargs)
+            redis_conn.set(redkey, json.dumps(result))
+            return result
+        return wrapper
     return decorator
