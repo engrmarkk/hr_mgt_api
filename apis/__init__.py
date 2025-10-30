cd43b096c59deceef38e46698a745972575d9655
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2025-10-30 13:42:06 +0100

    Add admin_router import to API package
    Imported admin_router in apis/__init__.py to make the admin routes available in the API package.

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,3 +2,4 @@ from apis.ping import ping_router
 from apis.users import user_router
 from apis.authentications import auth_router
 from apis.cloud_nary import cloudinary_router
+from apis.admins import admin_router
