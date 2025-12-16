7e2c27b21facc0aee0e769c9a8d6613e81468391
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2025-12-16 12:59:08 +0100

    Update __init__.py

--- apis/admins/__init__.py
+++ apis/admins/__init__.py
@@ -6,7 +6,7 @@ import traceback
 from models import Users
 from utils.rate_limit import limiter
 from logger import logger
-from cruds import create_leave_type
+from cruds import create_leave_type, create_one_dpt, department_exists
 from constants import EXCEPTION_MESSAGE
 
 
@@ -26,7 +26,37 @@ async def create_leavetype(
         leave_type = await create_leave_type(
             db, current_user.organization_id, leave_type_name, int(leave_type_duration)
         )
-        return {"msg": "Leave type created successfully"}
+        return {"detail": "Leave type created successfully"}
+    except HTTPException as http_exc:
+        # Log the HTTPException if needed
+        logger.exception("traceback from login")
+        raise http_exc
+    except Exception as e:
+        logger.exception("traceback from login")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=EXCEPTION_MESSAGE
+        )
+
+
+# create department
+@admin_router.post("/create_department", status_code=status.HTTP_200_OK)
+async def create_department(
+    request: Request,
+    current_user: Users = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    try:
+        data = await request.json()
+        department_name = data.get("name")
+
+        if await department_exists(db, department_name, current_user.organization_id):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Department already exists",
+            )
+
+        await create_one_dpt(db, current_user.organization_id, department_name)
+        return {"detail": "Department created successfully"}
     except HTTPException as http_exc:
         # Log the HTTPException if needed
         logger.exception("traceback from login")
