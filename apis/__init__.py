45c49a72e3f7d4f528aec381d30153ed1a268246
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2026-06-01 15:50:51 +0100

    Update __init__.py

--- apis/__init__.py
+++ apis/__init__.py
@@ -4,3 +4,4 @@ from apis.authentications import auth_router
 from apis.cloud_nary import cloudinary_router
 from apis.admins import admin_router
 from apis.settings import settings_router
+from apis.organization import org_router
